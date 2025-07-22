f7bb542735a5a7380227a11dc0ef3dfe5c6eb136
Abdelhalim Yasser <ABDELHALIMYASSER88@GMAIL.COM>
2025-07-22 13:08:56 +0300

    Update Python_version.py

--- other-languages/Python_version.py
+++ other-languages/Python_version.py
@@ -1,1 +1,11 @@
+import re
 
+def countWord(word, lyrics):
+    words = re.findall(rf"(?<!\w){re.escape(word)}(?!\w)", re.sub(r"[^\w\s\u0600-\u06FF]", "", lyrics), re.IGNORECASE)
+    count = len(words)
+
+    print(f"The word '{word}' appears {count} times in the lyrics.")
+
+lyrics = input("Paste your lyrics here (can be Arabic or English)...\n").strip()
+word = input("Enter the word to count: ").strip()
+countWord(word, lyrics)
